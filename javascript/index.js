147eb26d1ed752e119772b4554577be3630ebf67
PusaTeam <46245479+PusaTeam@users.noreply.github.com>
2020-01-04 18:48:45 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,9 @@
 document.body.onload = function () {
   let from = document.getElementById("from")
   , fromUl = document.getElementById("from__ul");
+  fromUl.focus();
+
+  
 
   let to = document.getElementById("to")
   , toUl = document.getElementById("to__ul");
@@ -27,7 +30,7 @@ document.body.onload = function () {
     debouncer: null,
   };
 
-  getJSON("javascript/city.json", function (err, result) {
+  getJSON("javascript/Prime_airports.json", function (err, result) {
     if (err != null) {
       alert("Something went wrong: " + err);
     } else {
@@ -35,41 +38,49 @@ document.body.onload = function () {
       let options = {
         shouldSort: true,
         tokenize: true,
-        threshold: 0.1,
+        threshold: 0.2,
         location: 0,
         distance: 500,
         maxPatternLength: 32,
         minMatchCharLength: 2,
         keys: [
-          //"iata_code",
-          //"name_rus",
-          //"name_eng",
+          "iata_code",
+          "name_rus",
+          "name_eng",
           "city_rus",
           "city_eng",
-          //"country_rus",
-          //"country_eng",
+          "country_rus",
+          "country_eng",
         ]
 
       };
       state.fuse = new Fuse(state.data, options);
     }
   });
+  
 
+ 
   function autocompleteFrom() {
     let fromV = from.value.trim();
     fromUl.classList.add("hidden");
     if (fromV.length) {
       let fromPlace = state.fuse.search(fromV);
       fromUl.innerHTML = "";
-      fromUl.classList.remove("hidden");
+      if(fromPlace.iata_code != ""){fromUl.classList.remove("hidden");}
+      if (!fromUl.children){
+        alert("asd")
+        fromUl.style.boxShadow="none";
+        fromUl.style.webkitBoxShadow="none";
+    }
+      
 
 
-      for (let i = 0; i < 5; i++) {
+     for (let i = 0; i < 5; i++) {
 
         if (!fromPlace[i]) continue;
 
         if (i == 0) {
-          fromUl.innerHTML = `<span>` +
+          fromUl.innerHTML = `<li>` + `<span>` +
             (( fromPlace[0].city_rus ? 
               fromPlace[0].city_rus :
                 fromPlace[0].city_eng) + `</span>`
@@ -78,7 +89,7 @@ document.body.onload = function () {
                 fromPlace[0].country_rus :
                 fromPlace[0].country_eng)
                 + `<span>` +
-              " Все аэропорты" + '</span>' +'</span>'
+              " Все аэропорты" + '</span>' +'</span>' + `</li>`
             )
 
 
@@ -103,9 +114,14 @@ document.body.onload = function () {
             fromPlace[i].city_rus :
             fromPlace[i].city_eng) + '</span>'
       }
+      
     }
 
-  }
+       }
+    
+
+  
+
 
   
 
@@ -119,10 +135,17 @@ document.body.onload = function () {
   }
 
   fromUl.onclick = function(e){
-    let target = e.target.closest("li");
-    let textTarget = target.firstChild.firstChild.textContent;
-    let textCity = fromUl.firstChild.textContent 
-    from.value = fromUl.firstChild.textContent + "   (" + target.firstChild.firstChild.textContent +")";
+    e.stopPropagation();   
+    let target = e.target.closest("li")  
+    let textCity = target.lastChild.textContent;
+    let airpotName = target.firstChild.firstChild.textContent;
+    if (target == fromUl.firstChild){
+      from.value = airpotName + " (Все аэропорты)";
+    }  
+    else from.value = airpotName ;
+    if(from.value.length > 27){
+      from.value = airpotName.slice(0, 27) + "..."
+    }
     fromUl.classList.add("hidden");
   }
 
@@ -134,7 +157,7 @@ document.body.onload = function () {
     if (toV.length) {
       let toPlace = state.fuse.search(toV);
       toUl.innerHTML = "";
-      toUl.classList.remove("hidden");
+      if(toPlace[0].city_eng != ""){toUl.classList.remove("hidden");}
 
 
       for (let i = 0; i < 5; i++) {
@@ -142,7 +165,7 @@ document.body.onload = function () {
         if (!toPlace[i]) continue;
 
         if (i == 0) {
-          toUl.innerHTML = `<span>` +
+          toUl.innerHTML = `<li>` + `<span>` +
             (( toPlace[0].city_rus ? 
               toPlace[0].city_rus :
               toPlace[0].city_eng) + `</span>`
@@ -151,7 +174,7 @@ document.body.onload = function () {
                 toPlace[0].country_rus :
                 toPlace[0].country_eng)
                 + `<span>` +
-              " Все аэропорты" + '</span>' +'</span>'
+              " Все аэропорты" + '</span>' +'</span>' + `</li>`
             )
 
 
@@ -182,10 +205,14 @@ document.body.onload = function () {
 
 
   toUl.onclick = function(e){
-    let target = e.target.closest("li");
-    let textTarget = target.firstChild.firstChild.textContent;
-    let textCity = toUl.firstChild.textContent 
-    to.value = toUl.firstChild.textContent + " (" + target.firstChild.firstChild.textContent +")";
+    e.stopPropagation();   
+    let target = e.target.closest("li")  
+    let textCity = target.lastChild.textContent;
+    let airpotName = target.firstChild.firstChild.textContent;
+    if (target == toUl.firstChild){
+      to.value = airpotName + " (Все аэропорты)"
+    }  
+    else to.value = airpotName ;
     toUl.classList.add("hidden");
   }
 
@@ -197,16 +224,27 @@ document.body.onload = function () {
     state.debouncer = setTimeout(autocompleteTo, 300);
   };
   
+ /*  to.onblur = function(){
+    toUl.classList.add("hidden")
+  } */
 
 
-  let helpooBTN = document.getElementById("helpBTN"),
+  let helpBTN = document.getElementById("helpBTN"),
     dropMenu = document.getElementById("dropMenu");
 
-  helpooBTN.onclick = function () {
-    dropMenu.classList.toggle("hidden")
+  helpBTN.onclick = function (e) {
+   
+    dropMenu.classList.remove("hidden");
+  
+
   };
 
-
+  document.onclick= function(e){
+    if (e.target != helpBTN && e.target != helpBTN.lastChild){
+      dropMenu.classList.add("hidden");
+    }
+   }
+ 
 
 
 }
@@ -275,3 +313,82 @@ function formatDate(date) {
           'YYYY-MM-DD'));
       });
     });
+
+
+    let fromToSwitch = document.getElementById("switch");
+
+    fromToSwitch.onclick = function(e){     
+      let str = "";
+      str = from.value;
+      from.value = to.value;
+      to.value = str;     
+    }
+
+    document.addEventListener("DOMContentLoaded", function(){
+      var el = document.querySelector(".button-bird");
+      var text = document.querySelector(".button-bird__text");
+          el.addEventListener('click', function() {
+            el.classList.toggle('active');
+
+            if(el.classList.contains('active')){
+            	console.log('true');
+            	text.innerHTML = 'DONE';
+            }else{
+            	text.innerHTML = 'SEND';
+            }
+        });
+    });
+
+    $(function () { 
+      var $popover = $('.popover-markup>.trigger').popover({
+        html: true,
+        placement: 'bottom',
+        // title: '<?= lang("Select passengers");?><a class="close demise");">×</a>',
+        content: function () {
+            return $(this).parent().find('.content').html();
+        }
+      });
+  
+      // open popover & inital value in form
+      var passengers = [1,0,0];
+      $('.popover-markup>.trigger').click(function (e) {
+          e.stopPropagation();
+          $(".popover-content input").each(function(i) {
+              $(this).val(passengers[i]);
+          });
+      });
+      // close popover
+      $(document).click(function (e) {
+          if ($(e.target).is('.demise')) {        
+              $('.popover-markup>.trigger').popover('hide');
+          }
+      });
+  // store form value when popover closed
+   $popover.on('hide.bs.popover', function () {
+      $(".popover-content input").each(function(i) {
+          passengers[i] = $(this).val();
+      });
+    });
+      // spinner(+-btn to change value) & total to parent input 
+      $(document).on('click', '.number-spinner a', function () {
+          var btn = $(this),
+          input = btn.closest('.number-spinner').find('input'),
+          total = $('#passengers').val(),
+          oldValue = input.val().trim();
+  
+      if (btn.attr('data-dir') == 'up') {
+        if(oldValue < input.attr('max')){
+          oldValue++;
+          total++;
+        }
+      } else {
+        if (oldValue > input.attr('min')) {
+          oldValue--;
+          total--;
+        }
+      }
+      $('#passengers').val(total);
+      input.val(oldValue);
+    });
+    $(".popover-markup>.trigger").popover('show');
+  });
